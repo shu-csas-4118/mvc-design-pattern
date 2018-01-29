2eb743d8a74d7237ac70e0c4d27c09d7f7e756e9
Weeks4397 <35846723+Weeks4397@users.noreply.github.com>
2018-01-29 18:59:27 -0500

    Update StudentController.java
    i figured that this should be the only class using student and student view so i added all the student function into this class

--- src/StudentController.java
+++ src/StudentController.java
@@ -1,14 +1,57 @@
 
-public class StudentController {
-	private StudentView studentView;
-	private Student	student;
+public class studentController {
+
+	private studentView SV;
+	private student S;
 	
-	public StudentController(Student student, StudentView studentView) {
-		this.student = student;
-		this.studentView = studentView;
+	public studentController (student s, studentView sv) {
+		
+		this.SV = sv;
+		this.S = s;
 	}
 	
 	public void printStudentDetails() {
-		this.studentView.printStudentDetails(this.student);
+		this.SV.printStutentDetails();
+	}
+	
+	public String getfirstname() {
+		return this.S.getfirstname();
+	}
+	
+	public void setfirstname(String F) {
+		this.S.setfirstname(F);
+	}
+	
+	public String getlastname() {
+		return this.S.getlastname();
+	}
+	
+	public void setlastname(String L) {
+		this.S.setlastname(L); 
+	}
+	
+	public int getIDNumber() {
+		return this.S.getIDnumber();
+	}
+	
+	public void setIDNumber(int NewNumber) {
+		this.S.setIDnumber(NewNumber);
+	}
+	public String getEmail() {
+		return this.S.getEmail();
+	}
+	
+	public void setemail(String e) {
+		this.S.setemail(e);
+	}
+	
+	public courseControllor getCourse(int courselocation) {
+		
+		return this.S.getCourse(courselocation);
+	}
+	public void addCourse(courseControllor C) {
+		
+		this.S.addCourse(C);
 	}
 }
+
